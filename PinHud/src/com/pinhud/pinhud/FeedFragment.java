a126654aa5ed297241014aeee69bfb13d1f9cb8f
Austin Browne <dazztrazak@gmail.com>
2014-01-18 19:04:58 -0500

    gdgsgx

--- PinHud/src/com/pinhud/pinhud/FeedFragment.java
+++ PinHud/src/com/pinhud/pinhud/FeedFragment.java
@@ -14,33 +14,43 @@ import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class FeedFragment extends Fragment {
 
-		private GridLayout layout;
-		private RelativeLayout newCard;
-		private RelativeLayout newerCard;
-		
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	            Bundle savedInstanceState) {
-	        // Inflate the layout for this fragment
-	        View view = inflater.inflate(R.layout.feed_layout, container,
-	                false);
-	        
-	        layout = (GridLayout) view.findViewById(R.id.theLayout);
-	        for (int i = 0; i < 3; i++){
-	        	createCard();
-	        }
-	        
-			
-	        return view;
-	    }
-		public void createCard(){
-			newCard = (RelativeLayout) View.inflate(this.getActivity(), R.layout.main_list_card, null);
-	        newerCard = (RelativeLayout) View.inflate(this.getActivity(), R.layout.main_list_card, null);
-			layout.addView(newCard);
-	        layout.addView(newerCard);
+	private GridLayout layout;
+	private RelativeLayout newCard;
+	private TextView title;
+	private TextView info;
+	private ImageView pic;
+	private View view;
+
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		view = inflater.inflate(R.layout.feed_layout, container,
+				false);
+
+		title = (TextView) view.findViewById(R.id.pinTitle);
+		info = (TextView) view.findViewById(R.id.pinDescription);
+		layout = (GridLayout) view.findViewById(R.id.theLayout);
+
+		for (int i = 0; i < 3; i++){
+			createCard();
 		}
+
+
+		return view;
+	}
+	public void createCard(){
+
+		newCard = (RelativeLayout) View.inflate(this.getActivity(), R.layout.main_list_card, null);
+		title.setText("An Item");
+		info.setText("This is a fucking item. Deal with it.");
+		layout.addView(newCard);
+	}
 }
